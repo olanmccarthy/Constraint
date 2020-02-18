0e2d92424845465576cf306926a2fc8aea4ce216
olan <olan@olan.com>
2020-02-18 15:30:18 +0000

    added imports

--- src/assignment2/Residents116443264.java
+++ src/assignment2/Residents116443264.java
@@ -1,4 +1,8 @@
 package assignment2;
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.variables.IntVar;
 
 public class Residents116443264 {
+
 }
